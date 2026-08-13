549f5cde859cacd096cfced13f05d3ff84eb0690
moeendres-png <moe.endres@gmail.com>
2026-08-13 21:35:52 +0200

    Remove Phase 3 transport staging file

--- PHASE3_STAGING_gate_probe.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from collections import Counter
-
-
-def validate_counts(mainboard: tuple[str, ...]) -> tuple[int, dict[str, int]]:
-    return len(mainboard), dict(Counter(mainboard))
