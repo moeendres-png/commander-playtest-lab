94372dc787c4c4aab1d40246dadccdcf7cf96913
moeendres-png <moe.endres@gmail.com>
2026-08-14 02:14:45 +0200

    Activate Whole-Deck public workflow adapter

--- src/commander_lab/tools/__init__.py
+++ src/commander_lab/tools/__init__.py
@@ -2,6 +2,11 @@ from .current_candidates import canonical_feature_fusion_summary, load_candidate
 from .local_snapshots import build_local_snapshots
 from .registry import PUBLIC_TOOL_DEFINITIONS, TOOL_DEFINITIONS, ToolDefinition, ToolRegistry
 from .service import ApprovalRequired, CommanderToolService, ToolExecutionError
+from .whole_deck_public import install_whole_deck_public_integration
+
+install_whole_deck_public_integration()
+from . import registry as _registry_module
+PUBLIC_TOOL_DEFINITIONS = _registry_module.PUBLIC_TOOL_DEFINITIONS
 
 __all__ = [
     "PUBLIC_TOOL_DEFINITIONS",
