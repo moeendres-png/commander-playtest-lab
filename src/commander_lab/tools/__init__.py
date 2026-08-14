a4d9e4857c7593914f8246dfee9285c3b2b067c6
moeendres-png <moe.endres@gmail.com>
2026-08-14 02:21:14 +0200

    Fix public tooling import integration

--- src/commander_lab/tools/__init__.py
+++ src/commander_lab/tools/__init__.py
@@ -1,11 +1,11 @@
+from . import registry as _registry_module
 from .current_candidates import canonical_feature_fusion_summary, load_candidate_profiles
 from .local_snapshots import build_local_snapshots
-from .registry import PUBLIC_TOOL_DEFINITIONS, TOOL_DEFINITIONS, ToolDefinition, ToolRegistry
+from .registry import TOOL_DEFINITIONS, ToolDefinition, ToolRegistry
 from .service import ApprovalRequired, CommanderToolService, ToolExecutionError
 from .whole_deck_public import install_whole_deck_public_integration
 
 install_whole_deck_public_integration()
-from . import registry as _registry_module
 PUBLIC_TOOL_DEFINITIONS = _registry_module.PUBLIC_TOOL_DEFINITIONS
 
 __all__ = [
