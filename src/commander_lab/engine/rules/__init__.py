a8ab0b62225f31dff8e97f43f37a2f0c828242dc
moeendres-png <moe.endres@gmail.com>
2026-08-24 08:35:14 +0200

    feat: expose XMage full-game runner contracts

--- src/commander_lab/engine/rules/__init__.py
+++ src/commander_lab/engine/rules/__init__.py
@@ -5,6 +5,25 @@ from .base import (
     RulesEngineUnavailable,
 )
 from .bridge import ExternalRulesAdapter, ForgeRulesAdapter, JsonLineBridgeClient, XMageRulesAdapter
+from .full_game import (
+    FULL_GAME_DECISION_PROTOCOL_VERSION,
+    FULL_GAME_EVIDENCE_CLASS,
+    FULL_GAME_LANE,
+    ExternalPilotDecisionPolicy,
+    FullGameConformanceError,
+    FullGameConformanceResult,
+    FullGamePilotBinding,
+    FullGameProtocolError,
+    FullGameReplayGate,
+    XmageFullGameRunner,
+)
+from .full_game_batch import (
+    FullGameBatchCase,
+    FullGameBatchRecord,
+    FullGameBatchReport,
+    FullGameFailureClass,
+    XmageFullGameBatchRunner,
+)
 from .manager import RulesEngineManager
 from .phase85 import PHASE85_VERSION, run_phase85_validation
 from .project import load_project_rules_decks, load_rules_deck_snapshot
@@ -20,10 +39,23 @@ from .tactical import TacticalRuleError, TacticalRuleOracle, TacticalRulesAdapte
 from .validation import PHASE8_ENGINE_VERSION, run_phase8_validation
 
 __all__ = [
+    "FULL_GAME_DECISION_PROTOCOL_VERSION",
+    "FULL_GAME_EVIDENCE_CLASS",
+    "FULL_GAME_LANE",
     "PHASE8_ENGINE_VERSION",
     "PHASE85_VERSION",
+    "ExternalPilotDecisionPolicy",
     "ExternalRulesAdapter",
     "ForgeRulesAdapter",
+    "FullGameBatchCase",
+    "FullGameBatchRecord",
+    "FullGameBatchReport",
+    "FullGameConformanceError",
+    "FullGameConformanceResult",
+    "FullGameFailureClass",
+    "FullGamePilotBinding",
+    "FullGameProtocolError",
+    "FullGameReplayGate",
     "JsonLineBridgeClient",
     "ReplayValidationError",
     "ReplayValidationResult",
@@ -36,6 +68,8 @@ __all__ = [
     "TacticalRuleOracle",
     "TacticalRulesAdapter",
     "XMageRulesAdapter",
+    "XmageFullGameBatchRunner",
+    "XmageFullGameRunner",
     "build_protocol_schema",
     "build_validation_registry",
     "load_interaction_catalog",
